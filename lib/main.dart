35d4a7d1d569f9c8c74d29ff29c22c76891263f8
Dikki AP <dikki.1710@gmail.com>
2023-03-30 00:15:36 +0700

    3 - Text Widget

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,22 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: const Text('Flutter Basic'),
         ),
-        body: const Center(child: Text('Flutter Basic')),
+        body: Center(
+            child: Container(
+              width: 200,
+              height: 150,
+              padding: const EdgeInsets.all(16.0),
+              color: Colors.lightBlue,
+              child: const Text(
+                      'This is example paragraph for Widget text in Flutter Basic App',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.w500,
+                        color: Colors.white
+                      ),
+                    ),
+            )),
       ),
     );
   }
