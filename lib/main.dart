e6587339dc038addfc57df3036d7c8455f60d2a0
Dikki AP <dikki.1710@gmail.com>
2023-03-30 14:35:26 +0700

    13 - Stack & Align

--- lib/main.dart
+++ lib/main.dart
@@ -8,67 +8,102 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  /*
-    -- Flexible Widget --
-    So when u want to make a proportional size of Widget, u can use Flexible
-    u can set total size amount of Widget
-    For example in this case study, we make a Column that have 3 Container
-    - First one is 1/4 from Device Height
-    - Second one is 2/4 or (1/2) from Device Height
-    - Third one is 1/4 from Device Height
-
-    So u can total it 1/4 + 2/4 + 1/4 = 4/4
-    total amount is 4, so u can input in into "flex" property on Flexible Widget
-  */
-
   @override
   Widget build(BuildContext context) {
+    List<Widget> sampleText = [];
+
+    for (var i = 1; i <= 25; i++) {
+      sampleText.add(Text(
+        'This is sample text $i',
+        style: TextStyle(fontSize: 36),
+      ));
+    }
+
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('Flexible Widget'),
-        ),
-        body: Column(
-          children: [
-            Flexible(
-                flex: 1, // Make a part that has 1 Flex from total 4 Flex
-                child: Row(
-                  children: [
-                    Flexible(
-                        flex: 1,
-                        child: Container(
-                          margin: EdgeInsets.all(10),
-                          color: Colors.orange,
-                        )),
-                    Flexible(
-                        flex: 1,
-                        child: Container(
-                          margin: EdgeInsets.all(10),
-                          color: Colors.lightBlue,
-                        )),
-                    Flexible(
-                        flex: 1,
-                        child: Container(
-                          margin: EdgeInsets.all(10),
-                          color: Colors.green,
-                        )),
-                  ],
-                )),
-            Flexible(
-                flex: 2, // Make a part that has 2 Flex from total 4 Flex
-                child: Container(
-                  margin: EdgeInsets.all(10),
-                  color: Colors.red,
-                )),
-            Flexible(
-                flex: 1, // Make a part that has 1 Flex from total 4 Flex
-                child: Container(
-                  margin: EdgeInsets.all(10),
-                  color: Colors.blue,
-                )),
-          ],
-        ),
-      ),
+          appBar: AppBar(
+            title: Text('Stack & Align'),
+          ),
+          /*
+            -- Stack --
+            Like a stacking Widget, first Widget will be the base Layout (Bottom)
+           */
+          body: Stack(
+            children: [
+              // First Layout (Base)
+              Column(
+                children: [
+                  Flexible(
+                      flex: 1,
+                      child: Row(
+                        children: [
+                          Flexible(
+                            flex: 1,
+                            child: Container(
+                              color: Colors.white,
+                            ),
+                          ),
+                          Flexible(
+                            flex: 1,
+                            child: Container(
+                              color: Colors.black12,
+                            ),
+                          ),
+                        ],
+                      )),
+                  Flexible(
+                      flex: 1,
+                      child: Row(
+                        children: [
+                          Flexible(
+                            flex: 1,
+                            child: Container(
+                              color: Colors.black12,
+                            ),
+                          ),
+                          Flexible(
+                            flex: 1,
+                            child: Container(
+                              color: Colors.white,
+                            ),
+                          ),
+                        ],
+                      ))
+                ],
+              ),
+
+              // Second Layout
+              ListView(
+                children: [
+                  Column(
+                    children: sampleText,
+                  )
+                ],
+              ),
+
+              // Third Layout
+              Align(
+                  /*
+                    --- Align --
+                    "alignment" property need 2 params "x" and "y"
+
+                    For x:
+                    1: Top
+                    0: Middle
+                    -1: Bottom
+
+                    For y:
+                    1: Bottom
+                    0: Middle
+                    -1: Top
+
+                    If u want to adjust the position u can use custom number like double
+                  */
+                  alignment: Alignment(0, 0.75),
+                  child: ElevatedButton(
+                      onPressed: () {}, child: Text('Example Button')))
+            ],
+          )),
     );
   }
 }
