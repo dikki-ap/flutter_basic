cceffe6a5a164f1d41defae351fc794092c067ab
Dikki AP <dikki.1710@gmail.com>
2023-03-30 15:47:14 +0700

    16 - Draggable, DragTarget, SizedBox, Material

--- lib/main.dart
+++ lib/main.dart
@@ -5,52 +5,165 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  // Declare a default Color
+  Color color1 = Colors.red;
+  Color color2 = Colors.green;
+
+  // Declare targetColor
+  Color? targetColor;
+
+  // Declare condition when DragTarget already accepting value or not
+  bool isAccepted = false;
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-          appBar: AppBar(
-            title: Text('Spacer Widget'),
-          ),
-          body: Center(
-            child: Row(
-              // mainAxisAlignment: MainAxisAlignment.spaceBetween, // This will be give space between Widget for the same amount
+        appBar: AppBar(
+          title: Text('Draggable, Dragtarget, SizedBox, Material'),
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: [
-                Spacer(
-                  flex: 1,
-                ), // Or u can use "flex" property on Spacer to make it Flexible
-                Container(
-                  width: 80,
-                  height: 80,
-                  color: Colors.red,
+                /*
+                  -- Draggable --
+                  Draggable need Generic Type, in this case we will use Color as a Generic Type
+                  Need 3 Properties
+                  "child" as a initial value
+                  "childWhenDragging" as a initialValue when u dragging the Object
+                  "feedback" as a Object when u dragging
+                */
+                Draggable<Color>(
+                  data: color1,
+                  feedback: SizedBox(
+                    width: 50,
+                    height: 50,
+                    child: Material(
+                      color: color1.withOpacity(0.6),
+                      shape:
+                          StadiumBorder(), // Make a Rectangle corner into Circular
+                      elevation: 1, // DROP SHADOW
+                    ),
+                  ),
+                  childWhenDragging: SizedBox(
+                    width: 50,
+                    height: 50,
+                    child: Material(
+                      color: Colors.grey,
+                      shape:
+                          StadiumBorder(), // Make a Rectangle corner into Circular
+                    ),
+                  ), // "data" property, is a data that belongs to Draggable and will be sent to a DragTarget
+                  child: SizedBox(
+                    width: 50,
+                    height: 50,
+                    child: Material(
+                      color: color1,
+                      shape:
+                          StadiumBorder(), // Make a Rectangle corner into Circular
+                      elevation: 3, // DROP SHADOW
+                    ),
+                  ),
                 ),
-                Spacer(
-                  flex: 2,
-                ), // Or u can use "flex" property on Spacer to make it Flexible
-                // Spacer(), // Or u can use Empty Spacer like this instead of spaceBetween
-                Container(
-                  width: 80,
-                  height: 80,
-                  color: Colors.green,
+                ElevatedButton(
+                    onPressed: () {
+                      setState(() {
+                        isAccepted = false;
+                      });
+                    },
+                    child: Text('Reset Target Color')),
+                Draggable<Color>(
+                  data: color2,
+                  feedback: SizedBox(
+                    width: 50,
+                    height: 50,
+                    child: Material(
+                      color: color2.withOpacity(0.6),
+                      shape:
+                          StadiumBorder(), // Make a Rectangle corner into Circular
+                      elevation: 1, // DROP SHADOW
+                    ),
+                  ),
+                  childWhenDragging: SizedBox(
+                    width: 50,
+                    height: 50,
+                    child: Material(
+                      color: Colors.grey,
+                      shape:
+                          StadiumBorder(), // Make a Rectangle corner into Circular
+                    ),
+                  ), // "data" property, is a data that belongs to Draggable and will be sent to a DragTarget
+                  child: SizedBox(
+                    width: 50,
+                    height: 50,
+                    child: Material(
+                      color: color2,
+                      shape:
+                          StadiumBorder(), // Make a Rectangle corner into Circular
+                      elevation: 3, // DROP SHADOW
+                    ),
+                  ),
                 ),
-                Spacer(
-                  flex: 3,
-                ), // Or u can use "flex" property on Spacer to make it Flexible
-                // Spacer(), // Or u can use Empty Spacer like this instead of spaceBetween
-                Container(
-                  width: 80,
-                  height: 80,
-                  color: Colors.blue,
-                ),
-                Spacer(
-                  flex: 2,
-                ), // Or u can use "flex" property on Spacer to make it Flexible
               ],
             ),
-          )),
+
+            /*
+              -- DragTarget --
+              DragTarget need Generic Type, in this case we using Color as Generic Type, same as Draggable "data"
+              DragTarget need 3 properties
+
+              "onWillAccept" , check if we want to accept the request from Draggable or not
+              "onAccept" , making a condition when accepting request from Draggable
+              "builder" , making a changes when accepting request from Draggable
+              "builder" , need 3 params, (context, caandidateData, rejectedData)
+            */
+            DragTarget<Color>(
+              onWillAccept: (value) => true, // Always accept changes
+              onAccept: (value) {
+                // When accepting request from Draggable, changes isAcceped and targetColor
+                isAccepted = true;
+                targetColor = value;
+              },
+              builder: (context, candidateData, rejectedData) {
+                // Making a changes when accepting request from Draggable
+                // Check isAccepted value, if True, change color to targetColor
+                // if False, make it to Grey Color
+                return (isAccepted)
+                    ? SizedBox(
+                        width: 100,
+                        height: 100,
+                        child: Material(
+                          color: targetColor,
+                          shape:
+                              StadiumBorder(), // Make a Rectangle corner into Circular
+                          elevation: 3, // DROP SHADOW
+                        ),
+                      )
+                    : SizedBox(
+                        width: 100,
+                        height: 100,
+                        child: Material(
+                          color: Colors.grey,
+                          shape:
+                              StadiumBorder(), // Make a Rectangle corner into Circular
+                        ),
+                      );
+              },
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
