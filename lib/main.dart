672ed4ac59f0f70e8a53aebee4f570a85f23af68
Dikki AP <dikki.1710@gmail.com>
2023-03-30 11:15:11 +0700

    4 - Row

--- lib/main.dart
+++ lib/main.dart
@@ -11,23 +11,19 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Flutter Basic'),
-        ),
-        body: Center(
-            child: Container(
-          width: 200,
-          height: 150,
-          padding: const EdgeInsets.all(16.0),
-          color: Colors.lightBlue,
-          child: const Text(
-            'This is example paragraph for Widget text in Flutter Basic App',
-            textAlign: TextAlign.center,
-            style: TextStyle(
-                fontSize: 24, fontWeight: FontWeight.w500, color: Colors.white),
+          appBar: AppBar(
+            title: const Text('Flutter Basic'),
           ),
-        )),
-      ),
+          body: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: const [
+              Text('Text 1'),
+              SizedBox(width: 8.0,),
+              Text('Text 2'),
+              SizedBox(width: 8.0,),
+              Text('Text 3'),
+            ],
+          )),
     );
   }
 }
