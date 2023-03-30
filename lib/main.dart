a60b73c4a88e1eed50d48e0870a14cdcdde3d3f4
Dikki AP <dikki.1710@gmail.com>
2023-03-30 12:20:54 +0700

    9 - TextStyle

--- lib/main.dart
+++ lib/main.dart
@@ -6,53 +6,43 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  int number = 0; // Declare number as int
-
-  // void addNumber() {
-  //   // Use setState to refresh page when button pressed, and update number value
-  //   setState(() {
-  //     number += 1;
-  //   });
-  // }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Anonymous Method on Stateful Widget'),
+          title: Text('Text Style'),
         ),
         body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                number.toString(),
-                style: TextStyle(
-                    fontSize: 10 +
-                        number
-                            .toDouble()), // Making fontSize grow bigger when button pressed
-              ),
-              ElevatedButton(
-                // U can use Anonymous Method when u just only make a function that used once
-                onPressed: () {
-                  setState(() {
-                    number += 1;
-                  });
-                }, // Running addNumber() function when button pressed
-                child: Text('Add'),
-              )
-            ],
-          ),
-        ),
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const [
+            Text(
+              'This is regular text',
+              style: TextStyle(
+                  fontFamily: 'Poppins',
+                  fontSize: 24,
+                  decoration: TextDecoration.overline,
+                  decorationColor: Colors.blue,
+                  decorationThickness: 5,
+                  decorationStyle: TextDecorationStyle.wavy),
+            ),
+            Text(
+              'This is italic text',
+              style: TextStyle(
+                  fontFamily: 'Poppins',
+                  fontStyle: FontStyle.italic,
+                  fontSize: 24,
+                  decoration: TextDecoration.underline,
+                  decorationColor: Colors.blue,
+                  decorationThickness: 5,
+                  decorationStyle: TextDecorationStyle.dashed),
+            ),
+          ],
+        )),
       ),
     );
   }
