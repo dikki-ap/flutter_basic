64b2c73cfdae3391e8aba38a889ebeca6e4a4a5a
Dikki AP <dikki.1710@gmail.com>
2023-03-31 11:40:14 +0700

    23 - InkWell Custom Button

--- lib/home.dart
+++ lib/home.dart
@@ -12,27 +12,58 @@ class HomePage extends StatelessWidget {
         .size
         .height; // Device Height (Exclude AppBar and StatusBar)
     final appBarHeight = AppBar().preferredSize.height; // AppBar Height
-    final statusBarHeight = MediaQuery.of(context).padding.top; // Status Bar Height
+    final statusBarHeight =
+        MediaQuery.of(context).padding.top; // Status Bar Height
 
     final bodyHeight = deviceHeight - appBarHeight - statusBarHeight;
 
     return Scaffold(
-        appBar: appBar('Media Query'),
-        body: Column(
-          children: [
-            Container(
-              color: Colors.amber,
-              width: MediaQuery.of(context).size.width /
-                  3, // 1/3 from Device Length
-              height: bodyHeight / 2, // 1/2 from Body Height
-            ),
-            Container(
-              color: Colors.green,
-              width: MediaQuery.of(context).size.width /
-                  3, // 1/3 from Device Length
-              height: bodyHeight / 2, // 1/2 from Body Height
-            ),
-          ],
+        appBar: appBar('InkWell Custom Button'),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              // Normal Button using ElevatedButton
+              ElevatedButton(onPressed: () {}, child: Text('Elevated Button')),
+
+              // InkWell Custom Button
+              Material(
+                borderRadius: BorderRadius.circular(
+                    24), // Make sure u have same borderRadius with Outer Style (Container)
+                elevation: 5,
+                child: Container(
+                  width: 150,
+                  height: 50,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(24),
+                    gradient: LinearGradient(
+                        colors: const [Colors.pink, Colors.purple],
+                        begin: Alignment.bottomLeft,
+                        end: Alignment.topRight),
+                  ),
+                  child: Material(
+                    borderRadius: BorderRadius.circular(
+                        24), // Make sure u have same borderRadius with Outer Style (Container)
+                    color: Colors
+                        .transparent, // U need to use this if u already make a custom Container, so Material doesn't override Container Style
+                    child: InkWell(
+                      borderRadius: BorderRadius.circular(
+                          24), // Make sure u have same borderRadius with Outer Style (Container)
+                      onTap: () {},
+                      splashColor: Colors
+                          .blue, // U can change splashColor when u hold the Button
+                      child: Center(
+                          child: Text(
+                        'InkWell Button',
+                        style: TextStyle(
+                            color: Colors.white, fontWeight: FontWeight.bold),
+                      )),
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
         ));
   }
 }
