d633dbf1ace379fa61811293c7961a1e152c427b
Dikki AP <dikki.1710@gmail.com>
2023-04-02 14:20:35 +0700

    35 - Shared Preferences

--- lib/home.dart
+++ lib/home.dart
@@ -1,8 +1,7 @@
 // ignore_for_file: prefer_const_constructors
 
 import 'package:flutter/material.dart';
-import 'package:flutter_basic/models/People.dart';
-import 'package:flutter_basic/models/User.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -12,7 +11,14 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  // TextEditingController for TextField Controller
+  TextEditingController nameController =
+      TextEditingController(text: 'Default Name');
+
+  // initialCondition for Switch
   bool isOn = false;
+
+  // initialValue for AnimatedSitcher "child"
   Widget myWidget = Container(
     width: 200,
     height: 100,
@@ -20,11 +26,37 @@ class _HomePageState extends State<HomePage> {
         color: Colors.red, border: Border.all(color: Colors.black, width: 3)),
   );
 
+  // U need to create a async Function because SharedPref need "await" keyword
+  void saveData() async {
+    // need "await" keyword because "getInstance()" is Future type
+    SharedPreferences pref = await SharedPreferences.getInstance();
+
+    // U can use "setXXX" method with "key" and "value"
+    pref.setString("name", nameController.text);
+    pref.setBool("isOn", isOn);
+  }
+
+  // LOAD DATA FUNCTION
+  Future<String> getName() async {
+    SharedPreferences pref = await SharedPreferences.getInstance();
+
+    // U need to add "??" operator to check if it's null, and give it the default value when it's null
+    return pref.getString("name") ?? "Default Name";
+  }
+
+  // LOAD DATA FUNCTION
+  Future<bool> getON() async {
+    SharedPreferences pref = await SharedPreferences.getInstance();
+
+    // U need to add "??" operator to check if it's null, and give it the default value when it's null
+    return pref.getBool("isOn") ?? false;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: Text('Switcher & Animated Switcher'),
+          title: Text('Shared Preferences'),
         ),
         body: Center(
           child: Column(
@@ -39,13 +71,18 @@ class _HomePageState extends State<HomePage> {
                 If u change the same widget, u need add "key" in the Widget
                 but, if u change from widget to another widget u don't need "key"
               */
-              AnimatedSwitcher(
-                duration: Duration(seconds: 1),
-                transitionBuilder: (child, animation) => RotationTransition(
-                  turns: animation,
-                  child: child,
-                ),
-                child: myWidget,
+              Column(
+                children: [
+                  AnimatedSwitcher(
+                    duration: Duration(seconds: 1),
+                    transitionBuilder: (child, animation) => RotationTransition(
+                      turns: animation,
+                      child: child,
+                    ),
+                    child: myWidget,
+                  ),
+                  TextField(controller: nameController)
+                ],
               ),
               Switch(
                   activeColor: Colors.green,
@@ -79,7 +116,32 @@ class _HomePageState extends State<HomePage> {
                         );
                       }
                     });
-                  })
+                  }),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  ElevatedButton(
+                      onPressed: () {
+                        saveData();
+                      },
+                      child: Text('Save')),
+                  ElevatedButton(
+                      onPressed: () {
+                        getName().then((value) {
+                          setState(() {
+                            nameController.text = value;
+                          });
+                        });
+
+                        getON().then((value) {
+                          setState(() {
+                            isOn = value;
+                          });
+                        });
+                      },
+                      child: Text('Load'))
+                ],
+              )
             ],
           ),
         ));
